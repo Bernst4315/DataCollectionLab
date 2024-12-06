924c1c7b44f1738a044a8e4d2f6b7a8f854a370f
bernst4315 <bernst4315@gmail.com>
2024-12-06 15:34:57 -0700

    updated code to accept varied array lengths on line 6. No longer hard coded

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,
 let arr = [];
 let arr2 = str.split("\n");
 
-for(i = 0; i < 5; i++){  
+for(i = 0; i < arr2.length; i++){  
     arr.push(arr2[i].split(","))
 }
 
