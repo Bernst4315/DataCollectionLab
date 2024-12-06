2348bf57eb4cc516c460522875edbe5d064182fb
bernst4315 <bernst4315@gmail.com>
2024-12-06 14:07:39 -0700

    copied old code from previous exercise

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Commit Test")
+const str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
+// let cell1 = "";
+// let cell2 = "";
+let row1 = ""; 
+let row2 = "";
+let row3 = "";
+let row4 = "";
+  let count = 0; 
+
+for (const char of str){
+    
+  if (char === "\n")count++;
+  if (char !== "\n" && count === 0) row1 = row1 + char;
+  if (char !== "\n" && count === 1) row2 = row2 + char; 
+  if (char !== "\n" && count === 2) row3 = row3 + char;
+  if (char !== "\n" && count === 3) row4 = row4 + char;
+  console.log(count, char);
+     
+}
+
+console.log("end " + count);
+console.log("row 1 " + row1);
+console.log("row 2 " + row2);
+console.log("row 3 " + row3);
+console.log("row 4 " + row4);
