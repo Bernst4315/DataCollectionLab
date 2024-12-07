0b7c48efe5b4dfbd02d4040984705f6959d373de
bernst4315 <bernst4315@gmail.com>
2024-12-07 11:56:25 -0700

    created loop using obj bracket notation to create an object with properties and values

--- labPart2.js
+++ labPart2.js
@@ -3,8 +3,29 @@ const str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,
 let arr = [];
 let arr2 = str.split("\n");
 
-for(i = 0; i < arr2.length; i++){  
+for(let i = 0; i < arr2.length; i++){  
     arr.push(arr2[i].split(","))
 }
 
 console.log(arr);
+
+let newArr = []; 
+let obj = {};
+
+for(let i = 0; i < arr[0].length; i++){
+    let prop = arr[0][i].toLowerCase();
+    let value = arr[1][i];
+    obj[prop] = value; 
+}
+
+//create new array to fill w/ objects
+//arr[0] - turn into properties for each object (spread operator might be used)
+//rest of arr values will need to e assigened to each objt prop
+
+// obj = {age: 0}
+// obj.age=10; 
+// const str2 = arr[0][0];
+
+// obj[str2.toLowerCase()] = arr[1][0];// bracket notation
+
+console.log(obj);
