458691b742bd6319328f0c6bc03683d1549d7774
bernst4315 <bernst4315@gmail.com>
2024-12-07 13:15:27 -0700

    Cleaned up code

--- labPart2.js
+++ labPart2.js
@@ -15,31 +15,15 @@ let obj = {};
 
 for(let i = 0; i < 4; i++){
     let newObj = {};
-    for(let j = 0; j < arr[0].length; j++){ //maybe try prop as inner loop and value as outer loop
+
+    for(let j = 0; j < arr[0].length; j++){ 
+
         let prop = arr[0][j].toLowerCase();
-     
-   // for(let j = 0; j < arr[i].length; j++){ 
         let value = arr[i+1][j];
         obj[prop] = value;
         newObj= {...obj};
-        
-//}
 
-//not working because obj is a value by ref. Need to use spreader to nake a new copy 
-//All I am doing is changing what is being refered to. 
-   
-}newArr.push(newObj);
+    }newArr.push(newObj);
 } 
 
-//create new array to fill w/ objects
-//arr[0] - turn into properties for each object (spread operator might be used)
-//rest of arr values will need to e assigened to each objt prop
-
-// obj = {age: 0}
-// obj.age=10; 
-// const str2 = arr[0][0];
-
-// obj[str2.toLowerCase()] = arr[1][0];// bracket notation
-
-//console.log(obj);
 console.log(newArr);
