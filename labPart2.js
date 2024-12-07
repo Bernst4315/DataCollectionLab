acba94f5df0518a38eb49ed91221ea6313c18120
bernst4315 <bernst4315@gmail.com>
2024-12-07 13:13:57 -0700

    added spreader operator to loop to make new obj copies

--- labPart2.js
+++ labPart2.js
@@ -1,5 +1,5 @@
 const str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26";
-
+//Turns String into an Array
 let arr = [];
 let arr2 = str.split("\n");
 
@@ -9,18 +9,27 @@ for(let i = 0; i < arr2.length; i++){
 
 console.log(arr);
 
+//Puts Array Values into an object and places them into a new array
 let newArr = []; 
 let obj = {};
 
-for(let i = 0; i < arr[0].length; i++){
-    let prop = arr[0][i].toLowerCase();
-    for(let j = 0; j < arr[0][j].length; j++){ 
-        let value = arr[1][i];
-        obj[prop] = value; 
-}
- newArr.push(obj);   
-}
+for(let i = 0; i < 4; i++){
+    let newObj = {};
+    for(let j = 0; j < arr[0].length; j++){ //maybe try prop as inner loop and value as outer loop
+        let prop = arr[0][j].toLowerCase();
+     
+   // for(let j = 0; j < arr[i].length; j++){ 
+        let value = arr[i+1][j];
+        obj[prop] = value;
+        newObj= {...obj};
+        
+//}
 
+//not working because obj is a value by ref. Need to use spreader to nake a new copy 
+//All I am doing is changing what is being refered to. 
+   
+}newArr.push(newObj);
+} 
 
 //create new array to fill w/ objects
 //arr[0] - turn into properties for each object (spread operator might be used)
@@ -32,5 +41,5 @@ for(let i = 0; i < arr[0].length; i++){
 
 // obj[str2.toLowerCase()] = arr[1][0];// bracket notation
 
-console.log(obj);
-console.log(newArr)
+//console.log(obj);
+console.log(newArr);
